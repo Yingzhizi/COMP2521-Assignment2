c8be036b19d5dbf5bf0c2d44e035cf6bbb290ea8
Yingzhi Zhou <z5125679@ad.unsw.edu.au>
2018-05-12 15:24:28 +1000

    complete PQ.c

---
+++ Graph.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Graph.h"
+
+
+typedef struct GraphRep {
+    int nV;
+    int nE;
+    adjListNode **edges;
+} GraphRep;
+
+/**
+  *Function is used to make a new graph
+  *
+  */
+Graph newGraph(int noNodes) {
+
+    //check to make sure the noNodes is valid
+    assert (noNodes >= 0);
+
+    //create a graph and make sure that it is valid
+    Graph g = malloc(sizeof(GraphRep));
+    assert (g != NULL);
+
+    //initialise the things in the struct
+    g->nV = noNodes;
+    g->nE = 0;
+
+    //create the array of edges and check to make sure it is valid
+    g->edges = malloc(noNodes * sizeof(adjListNode));
+    assert(g->edges != NULL);
+
+    //fill the edge array up
+    for (int i = 0; i < noNodes; i++)
+        g->edges[i] = NULL;
+    return g;
+}
+
+int validV(Graph g, Vertex v) {
+    return (g != NULL && v < g->nV && v >= 0);
+}
+
+void insertEdge(Graph g, Vertex src, Vertex dest, int weight) {
+    assert(g != NULL && validV(g, src) && validV(g, dest));
+    //if the edge is not in the graph
+    if (!inList(g->edges[src], dest)) {
+        g->edges[src] = insertLL(g->edges[src], dest, weight);
+    }
+    g->nE++;
+}
+//wo ai ni
+void  removeEdge(Graph g, Vertex src, Vertex dest) {
+    assert(g != NULL && validV(g, src) && validV(g, dest));
+    if (inList(g->edges[src], dest)) {
+        g->edges[src] = removeLL(g->edges[src], dest);
+    }
+    g->nE--;
+}
+
+bool  adjacent(Graph g, Vertex src, Vertex dest) {
+    assert(g != NULL && validV(g, src) && validV(g, dest));
+    return (inList(g->edges[src], dest));
+}
+
+int  numVerticies(Graph g) {
+    assert (g != NULL);
+    return g->nV;
+}
+
+/*
+ * Returns a list of adjacent vertices
+ * on outgoing edges from a given vertex.
+**/
+AdjList outIncident(Graph g, Vertex v) {
+    assert(g != NULL && validV(g, v));
+    return (g->edges[v]);
+}
+/*
+
+ * Returns a list of adjacent vertices
+ * on incoming edges from a given vertex.
+**/
+AdjList inIncident(Graph g, Vertex v) {
+    assert(g != NULL && validV(g, v));
+}
+
+
+void  showGraph(Graph g) {
+    assert (g != NULL);
+    int i;
+    printf("Number of vertices: %d\n", g->nV);
+    printf("Number of edges: %d\n", g->nE);
+    for (i = 0; i < g->nV; i++) {
+       printf("%d - ", i);
+       showLL(g->edges[i]);
+    }
+}
+
+void  freeGraph(Graph g) {
+    assert(g != NULL);
+    int i;
+    for (i = 0; i < g->nV; i++)
+       freeLL(g->edges[i]);
+    free(g->edges);
+    free(g);
+}
---
+++ PQ.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "PQ.h"
+#define INF 0x7FFFFFFF
+
+typedef struct PQRep {
+    struct PQNode *head;
+    struct PQNode *last;
+} PQRep;
+
+typedef struct PQNode {
+    ItemPQ item;
+    struct PQNode *next;
+} PQNode;
+
+/* Creates new priority queue, that can store items of type ItemPQ.
+*/
+PQ newPQ() {
+    PQRep *new = malloc(sizeof(PQRep));
+    assert (new != NULL);
+    new->head = NULL;
+    new->last = NULL;
+    return new;
+}
+
+/* Adds item (ItemPQ) to the priority queue.
+   If an item with 'key' exists, it's 'value' is updated.
+*/
+void addPQ(PQ q, ItemPQ item) {
+    assert (q != NULL);
+
+    PQNode *curr = q->head;
+    while (curr != NULL) {
+        if (curr->item->key == item->key) {
+            curr->item->value = item->value;
+            return;
+        }
+        curr = curr->next;
+    }
+
+    //If an item with 'key' doesn't exists
+    //we need to insert a new node
+    PQNode *new = malloc(sizeof(PQNode));
+    new->key = item->key;
+    new->value = item->value;
+
+    //if the q is empty
+    if (q->head == NULL) {
+        q->head = q->last = new;
+        return;
+    }
+
+    if (q->head->key > item->key) {
+        new->next = q->head;
+        q->head = new;
+        return;
+    } else if (q->last->key < item->key) {
+        q->last->next = new;
+        q->last = new;
+        return;
+    } else {
+        curr = q->head;
+        PQNode *prev = NULL;
+        while (curr != NULL) {
+            if (curr->item->key > item->key) {
+                prev->next = new;
+                new->next = curr;
+                break;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+}
+
+ItemPQ dequeuePQ(PQ q) {
+    assert (q != NULL);
+    //
+    if (PQEmpty(q)) return NULL;
+    PQNode *curr = q->head;
+
+    if(q->head == q->last){
+        q->head = q->last = NULL;
+        return curr->item;
+    } else {
+        int smallest = INF;
+        int keyIndex;
+        curr = q->head;
+        while(curr != NULL){
+            //get the smallest value and track the key
+            if(curr->item->value < smallest){
+                keyIndex = curr->item->key;
+                smallest = curr->item->value;
+            }
+            curr = curr->next;
+        }
+        PQNode *prev = NULL;
+        curr = q->head;
+        while(curr != NULL){
+            //remove the item
+            if(curr->item->key == keyIndex){
+                break;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+        PQNode * tmp = curr;
+        if (prev == NULL) {
+            q->head = curr->next;
+        } else if (curr == q->last) {
+            q->last = prev;
+            prev->next = NULL;
+        } else {
+            prev->next = curr->next;
+        }
+        free(curr);
+    }
+    return tmp;
+}
+
+void updatePQ(PQ q, ItemPQ item) {
+    assert (q != NULL);
+    PQNode *curr = q->head;
+    while (curr != NULL) {
+        if (curr->item->key == item->key) {
+            curr->item->value = item->value;
+        }
+        curr = curr->next;
+    }
+}
+
+int PQEmpty(PQ q) {
+     if(q->head == NULL){
+         return 1;
+     }
+     return 0;
+}
+
+void  showPQ(PQ q) {
+    PQNode *curr = q->head;
+    while(curr->next != NULL){
+        printf("%d > ", curr->item->value);
+        curr = curr->next;
+    }
+    printf("%d\n", curr->item->value);
+}
+
+void  freePQ(PQ q) {
+    PQNode *curr = q->head;
+    PQNode *tmp;
+    while (curr != NULL) {
+        tmp = curr->next;
+        free(curr->items);
+        curr = tmp;
+    }
+    free(q);
+}
